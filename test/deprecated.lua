fdcd23a1e8165c36b08efc116e61d8ce46514b1b
gigtih <104144818+gigtih@users.noreply.github.com>
2022-11-10 18:26:51 -0300

    examples

---
+++ test/deprecated.lua
@@ -0,0 +1,8 @@
+--[[ File deprecated with file deprecator by gigtih, https://github.com/gigtih/FileDeprecator ]]
+
+    game:findFirstChild("Hello")
+    game:service("Test")
+local ReplicatedStorage = game:service("ReplicatedStorage")
+local ServerScriptService = game:service("ServerScriptService")
+Wait(3)
+ServerScriptService:findFirstChild("Hello")
--- test/test.lua
+++ test/test.lua
@@ -1,1 +1,19 @@
-game:GetService("ReplicatedStorage")
+--[[
+    Testing comments
+
+    game:FindFirstChild("Hello")
+    game:GetService("Test")
+]]
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+task.wait(3)
+
+if ReplicatedStorage:WaitForChild("Test") ~= nil then
+    print("Hello")
+end
+do
+    ServerScriptService:WaitForChild("TestingEnd")
+end
+
+ServerScriptService:FindFirstChild("Hello")
