1b0ae37db7df2ce932d8f4015495c2a0e92d8a7f
gigtih <104144818+gigtih@users.noreply.github.com>
2022-11-11 18:54:27 -0300

    Fixed

--- src/main.py
+++ src/main.py
@@ -1,37 +1,48 @@
+# 11/09/2022
+# @gigtih
+
 functions_list = [
-    # "game",
     "GetService",
     "task.wait",
     "FindFirstChild"
+    # "game",
 ]
 
 DEPRECATOR_VERSION = 0.1
 
 selected = input("Welcome to the file deprecator. Please select a option: [D] to deprecate (must be a luau file), [V] for the version\n")
-lower_input = selected.lower()
+lowerinput = selected.lower().strip()
 
 def deprecate_file(pathToFile: str):
     if not(".lua" in pathToFile):
         raise("File does not contain .lua")
-    
+
     with open(pathToFile, "r") as file:
-        with open(input("Please insert the path & name of the deprecated file: ") + ".lua" , 'w') as deprecatedFile:
-            file_content = file.readlines()
-            for lineContent in file_content:
-                for functions in functions_list:
-                    if functions in lineContent:
-                        match functions:
-                            # case "game": deprecatedFile.write(lineContent.replace("game", "Game"))
-                            case "GetService": deprecatedFile.write(lineContent.replace("GetService", "service"))
-                            case "task.wait": deprecatedFile.write(lineContent.replace("task.wait", "Wait"))
-                            case "FindFirstChild": deprecatedFile.write(lineContent.replace("FindFirstChild", "findFirstChild"))
+        deprecated_file_path = input("Please insert the path & name of the deprecated file: ")
+        file_content = file.readlines()
+        print("\nDEPRECATING...")
+        with open(deprecated_file_path + ".lua", 'w') as deprecatedFile:
+            # deprecatedFile.write("--[[ File deprecated with file deprecator by gigtih, https://github.com/gigtih/FileDeprecator ]]\n\n")
+            # TODO: add game -> Game
+            for lineContent in file_content: 
+                if functions_list[0] in lineContent:
+                    deprecatedFile.write(lineContent.replace("game:GetService", "Game:service"))
+                elif functions_list[1] in lineContent:
+                    deprecatedFile.write(lineContent.replace("task.wait", "Wait"))
+                elif functions_list[2] in lineContent:
+                    deprecatedFile.write(lineContent.replace("FindFirstChild", "findFirstChild"))
+                else:
+                    deprecatedFile.write(lineContent)
 
             deprecatedFile.close()
 
         file.close()
+        print("\n{0} was successfully deprecated, deprecated file can be found at: {1}".format(file.name, deprecatedFile.name))
 
-if lower_input == 'd':
+    return
+
+if lowerinput == 'd':
     file_path = input("Great! Now please insert a path to a file: ")
-    deprecate_file(file_path)
-elif lower_input == 'v':
+    deprecate_file(file_path.strip())
+elif lowerinput == 'v':
     print("The current deprecator version is: " + str(DEPRECATOR_VERSION))
--- test/deprecated.lua
+++ test/deprecated.lua
@@ -1,8 +1,19 @@
---[[ File deprecated with file deprecator by gigtih, https://github.com/gigtih/FileDeprecator ]]
+--[[
+    Testing comments
 
     game:findFirstChild("Hello")
-    game:service("Test")
-local ReplicatedStorage = game:service("ReplicatedStorage")
-local ServerScriptService = game:service("ServerScriptService")
+    Game:service("Test")
+]]
+
+local ReplicatedStorage = Game:service("ReplicatedStorage")
+local ServerScriptService = Game:service("ServerScriptService")
 Wait(3)
+
+if ReplicatedStorage:WaitForChild("Test") ~= nil then
+    print("Hello")
+end
+do
+    ServerScriptService:WaitForChild("TestingEnd")
+end
+
 ServerScriptService:findFirstChild("Hello")
